b51bafdd3cf592d0699b553a37572099f3161d29
Mikko Niemitalo <mikko.niemitalo@gmail.com>
2016-08-20 10:14:06 +0800

    Add setup.py to allow local installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='algolib',
+      version='0.1',
+      description='Collection of algorithms and data structures',
+      url='http://github.com/niemmi/algolib',
+      author='Mikko Niemitalo',
+      license='MIT',
+      packages=['algolib'],
+      zip_safe=False)
